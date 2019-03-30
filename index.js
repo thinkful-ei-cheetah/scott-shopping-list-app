2f1258bdc783cb1abf811f42fb5d66b1d602dcdd
Scott Dowding <scottddowding1279@gmail.com>
2019-03-30 12:24:27 -0400

    complete shopping list functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+'use strict';
+/* global $ */
+
+$('#js-shopping-list-form').submit(function(event) {
+  event.preventDefault();  
+  let entry = $('#shopping-list-entry').val();
+  $('.shopping-list').append(` <li>
+         <span class="shopping-item">${entry}</span>
+         <div class="shopping-item-controls">
+           <button class="shopping-item-toggle">
+             <span class="button-label">check</span>
+           </button>
+           <button class="shopping-item-delete">
+             <span class="button-label">delete</span>
+           </button>
+         </div>
+       </li>`);
+});
+
+$('.shopping-list' ).on('click', '.shopping-item-toggle', function( event ) {
+  $(this)
+    .closest('li')
+    .find('.shopping-item')
+    .toggleClass('shopping-item__checked');
+});
+
+$('.shopping-list' ).on('click', '.shopping-item-delete', function( event ) {
+  $(this.closest('li')).remove();
+});
